3cf6f4d9e780712b4e8156cd3b0c539a4305c99e
fernandogs07 <fernandogs07@hotmail.com>
2022-05-19 15:46:04 -0300

    Melhorias na usabilidade

--- js/estudo.js
+++ js/estudo.js
@@ -1,8 +1,8 @@
-document.addEventListener('keypress', function(e){
-    if(e.key == "Enter" ){
+document.addEventListener('keypress', function (e) {
+    if (e.key == "Enter") {
         calcular();
     }
- }, false);
+}, false);
 
 // Cria a variável que rece a tabela
 var tablePrincipal = document.getElementById('table_alunos2')
@@ -11,7 +11,7 @@ var tablePrincipal = document.getElementById('table_alunos2')
 const lista = []
 
 
-function calcular(){
+function calcular() {
     // Declaração de variáveis
     const aluno = document.getElementById("aluno").value
     const nota1 = parseFloat(document.getElementById("nota1").value)
@@ -19,43 +19,45 @@ function calcular(){
     var media = parseFloat((nota1 + nota2) / 2)
     var situacao
 
-// Validação de campos em branco
-if(aluno=='' || isNaN(nota1) || isNaN(nota2)){
-    alert("Campos não podem ficar em branco")
-}else if(nota1>10 || nota2>10){
+    // Validação de campos em branco
+    if (aluno == '' || isNaN(nota1) || isNaN(nota2)) {
+        alert("Campos não podem ficar em branco")
+        formulario_aluno.aluno.focus();
+    } else if (nota1 > 10 || nota2 > 10) {
         nota1.value.innerHTML = ''
-        nota1.value.innerHTML=''
+        nota1.value.innerHTML = ''
+       
         // alert("As notas não podem ser maior que 10")
         // location = location
-       
-    }else{
-// verificação de Aprovado e Reprovado
-if(media >= 6.75){
-    situacao = "Aprovado"
-}else{
-    situacao = "Reprovado"
-}
-// insere os objetos de armazenamento dos dados dos alunos na array lista
-lista.push({'nome': aluno, 'nota1':nota1,'nota2':nota2,'media':media,'situacao':situacao})
 
-// Função que cria a linha com os dados
-const nomes = function(){
-    for(let i=0;i<lista.length;i++){
-        var tableHtml ='<tr>'+
-        '<td>'+lista[i].nome+'</td>'+
-        '<td>'+lista[i].nota1+'</td>'+
-        '<td>'+lista[i].nota2+'</td>'+
-        '<td>'+lista[i].media+'</td>'+
-        '<td>'+lista[i].situacao+'</td>'+
-    '</tr>'
-    
+    } else {
+        // verificação de Aprovado e Reprovado
+        if (media >= 6.75) {
+            situacao = "Aprovado"
+        } else {
+            situacao = "Reprovado"
+        }
+        // insere os objetos de armazenamento dos dados dos alunos na array lista
+        lista.push({ 'nome': aluno, 'nota1': nota1, 'nota2': nota2, 'media': media, 'situacao': situacao })
+
+        // Função que cria a linha com os dados
+        const nomes = function () {
+            for (let i = 0; i < lista.length; i++) {
+                var tableHtml = '<tr>' +
+                    '<td>' + lista[i].nome + '</td>' +
+                    '<td>' + lista[i].nota1 + '</td>' +
+                    '<td>' + lista[i].nota2 + '</td>' +
+                    '<td>' + lista[i].media + '</td>' +
+                    '<td>' + lista[i].situacao + '</td>' +
+                    '</tr>'
+
+            }
+            // Aqui jogamos a linha dentro da tabela
+            tablePrincipal.innerHTML += tableHtml
+        }
+        // Aqui chamamos a função nomes
+        nomes();
     }
-    // Aqui jogamos a linha dentro da tabela
-    tablePrincipal.innerHTML += tableHtml
-}
-// Aqui chamamos a função nomes
-nomes();
-}
 }
 
 
